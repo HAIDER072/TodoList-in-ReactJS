4819d073bdc47fc4359c9b36063870da5294f51f
Syed Abbas Haider Zaidi <abbashaider786000@gmail.com>
2024-10-12 17:45:23 +0530

    completed

--- src/components/ToDoItem.jsx
+++ src/components/ToDoItem.jsx
@@ -1,24 +1,42 @@
 import React,{useState} from "react";
 
-
 function ToDoItem(props) {
 
-    const [isDone,setDone]=useState(false)
+    
 
 
 
-    function handleClick() {
-        setDone((prev) => !prev)
-    }
+    
 
 
 
 
-    return <div onClick={handleClick}>
-        <li style={{ textDecoration: isDone ? "line-through" : "none" }} >
+    return <div onClick={()=>props.onChecked(props.id)}>
+        <li  >
             {props.text}
         </li>
         </div>
 }
 
 export default ToDoItem;
+// function ToDoItem(props) {
+
+//     const [isDone,setDone]=useState(false)
+
+
+
+//     function handleClick() {
+//         setDone((prev) => !prev)
+//     }
+
+
+
+
+//     return <div onClick={handleClick}>
+//         <li style={{ textDecoration: isDone ? "line-through" : "none" }} >
+//             {props.text}
+//         </li>
+//         </div>
+// }
+
+// export default ToDoItem;
